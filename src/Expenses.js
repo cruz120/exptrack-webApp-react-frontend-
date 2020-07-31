f04f84a30c96be41f3c8eee679a61f8954b7051d
Cruz120 <rcg@kth.se>
2020-07-31 16:38:46 +0200

    Updated UI form

--- src/Expenses.js
+++ src/Expenses.js
@@ -3,7 +3,7 @@ import AppNav from './AppNav';
 import DatePicker from 'react-datepicker';
 import './App.css';
 import "react-datepicker/dist/react-datepicker.css";
-import { Container, Label, Input, Form, FormGroup, Button} from 'react-bootstrap';
+import { Container,Col, Label, Input, Form, FormGroup, Button,FormRow ,InputGroup, FormControl, ControlLabel, Row} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 
 class Expenses extends Component {
@@ -17,54 +17,77 @@ class Expenses extends Component {
             <AppNav />
             <Container>
               {title}
+              
               <Form onSubmit={this.handleSubmit}>
-                <FormGroup>
-                  <label for="title">Title</label>
-                  <input
-                    type="text"
-                    name="title"
-                    id="title"
-                    onChange={this.handleChange}
-                    autoComplete="name"
-                  />
-                </FormGroup>
+               
+                <InputGroup className="mb-3"> 
+                  <InputGroup.Prepend>
+                        <InputGroup.Text id="inputGroup-sizing-sm">Title</InputGroup.Text>
+                  </InputGroup.Prepend>
+                            <FormControl
+                                  type="text"
+                                  placeholder="Title"
+                                  name="title"
+                                  id="title"
+                                  onChange={this.handleChange}
+                                  autoComplete="name">      
+                            </FormControl>
+                    </InputGroup>
+               
+                  <InputGroup className="mb-3"> 
+                  <InputGroup.Prepend>
+                        <InputGroup.Text id="inputGroup-sizing-sm">Category</InputGroup.Text>
+                  </InputGroup.Prepend>
+                            <FormControl
+                                  type="text"
+                                  placeholder="Category"
+                                  name="category"
+                                  id="category"
+                                  onChange={this.handleChange}
+                                  autoComplete="name">      
+                            </FormControl>
+                    </InputGroup>
 
-                <FormGroup>
-                  <label for="category">Category</label>
-                  <input
-                    type="text"
-                    name="category"
-                    id="category"
-                    onChange={this.handleChange}
-                  />
-                </FormGroup>
-
-                <FormGroup>
-                  <label for="expenseDate">Expense Date</label>
+                <InputGroup className="mb-3">
+                  <InputGroup.Prepend>
+                    <InputGroup.Text>Expense Date</InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <FormGroup size="lg">
                   <DatePicker
                     selected={this.state.date}
                     onChange={this.handleChange}
                   />
-                </FormGroup>
-                <div className="row">
-                  <FormGroup className="col-md-4 mb-3">
-                    <label for="location">Location</label>
-                    <input
-                      type="text"
-                      name="location"
-                      id="location"
-                      onChange={this.handleChange}
-                    />
                   </FormGroup>
-                </div>
-                <FormGroup>
+
+                  <InputGroup.Prepend>
+                    <InputGroup.Text>Price</InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <FormControl>
+
+                  </FormControl>
+                </InputGroup>
+
+                <InputGroup className="mb-3">
+                  <InputGroup.Prepend>
+                    <InputGroup.Text>Description</InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <FormControl
+                    type="text"
+                    name="location"
+                    id="location"
+                    onChange={this.handleChange}
+                          as="textarea" rows="3"
+                  />
+                </InputGroup>
+
+                <Form.Group>
                   <Button variant="primary" type="submit">
                     Save
                   </Button>{" "}
                   <Button variant="dark" tag={Link} to="/categories">
                     Cancel{" "}
                   </Button>
-                </FormGroup>
+                </Form.Group>
               </Form>
             </Container>
           </div>
